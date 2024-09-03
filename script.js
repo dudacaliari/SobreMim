f4cd6bd3fcb64737d6eb350c07a006f68a20b6fb
Eduarda Caliari <eduarda.dcaliari@gmail.com>
2024-09-02 22:13:57 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function scrollToTimeline() {
+    document.getElementById("timeline").scrollIntoView({ behavior: 'smooth' });
+}
+
+function downloadResume() {
+    window.open('Curriculo.pdf', '_blank');
+}
